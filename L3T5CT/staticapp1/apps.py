542ba72c392ca275cffad028f2a117dc358d5436
chatsim79 <120016778+chatsim79@users.noreply.github.com>
2022-12-08 20:08:31 +0000

    Delete L3T5CT directory

--- L3T5CT/staticapp1/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Staticapp1Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'staticapp1'
--- L3T5CT/staticapp1/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-# Create your viedef index(request):
-def index(request):
-    return render(request, "index.html")
